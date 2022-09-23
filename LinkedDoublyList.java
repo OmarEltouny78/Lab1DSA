7e0bf38dfb1a134045142cbee0fe30f86ac2d124
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-09-23 10:39:33 +0200

    Add files via upload

---
+++ LinkedDoublyList.java
@@ -0,0 +1,30 @@
+public class LinkedDoublyList<E> {
+    //starter node
+    Node<E> head;
+    private static class Node<E>{
+        // previous pointer
+        //data
+        //next pointer
+
+
+
+        public Node(E element) {
+
+        }
+        //get data function
+
+    }
+    //add to doubly linked list
+    public void push(int new_data)
+    {
+
+    }
+    //reverse a doubly linked list
+    public void reverse(){
+
+    }
+    void printList(Node node)
+    {
+    }
+
+}
---
+++ LinkedSinglyList.java
@@ -0,0 +1,28 @@
+public class LinkedSinglyList<E> {
+    //started or head node
+    Node<E>head;
+    private static class Node<E>{
+        //class Node
+        //data
+        // next pointer
+
+        public Node(E element) {
+        }
+
+     //get data function
+    }
+    public void middle(){
+        //get middle of a Linked List
+    }
+    public void printList(){
+        //print a linked list
+
+    }
+    public void push(E new_data){
+        //add value to linked list
+    }
+    public void reverse(Node node){
+        //reverse Linked List
+
+    }
+}
